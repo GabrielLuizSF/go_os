9dac44512f3208efa0c5872a768c1484531fa358
GabrielLuizSF <gabrielluizdeveloper7@gmail.com>
2022-07-21 23:46:50 -0300

    creator init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GabrielLuizSF/creator
+
+go 1.18
---
+++ services/dir.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+    "log";
+    "os";
+    "fmt";
+)
+
+func DirCreate() {
+
+
+ var dirname string = ""
+    fmt.Println("DIR NAME:\t");
+    fmt.Scanln(&dirname)
+    
+    folder := os.Mkdir("../" + dirname, 0755);
+    
+    if folder != nil {
+        log.Fatal(folder)
+    }
+
+   fmt.Println("Folder Created");
+}
