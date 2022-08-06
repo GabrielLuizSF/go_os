165c5b4a38452e687aba900e5da50ba9d3bbb76a
Gabriel Luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-08-05 22:04:15 -0300

    FileEraser

--- creator.go
+++ creator.go
@@ -12,7 +12,7 @@ func main(){
 
 func UserChoice(){
     var userinput int;
-    fmt.Println("\n\nOptions:\n 1-DirCreator\t 2-FileCreator\n");
+    fmt.Println("\n\nOptions:\n 1-DirCreator\t 2-FileCreator\t 3-FileEraser\n");
     fmt.Scanln(&userinput)
 
     switch userinput{
@@ -20,6 +20,8 @@ func UserChoice(){
         DirCreator()
         case 2:
         FileCreator()
+        case 3:
+        DeleteFile()
         default:
        fmt.Println(" NAN\n\n")
     }
@@ -35,9 +37,11 @@ func DirCreator() {
     
     if folder != nil {
         log.Fatal(folder)
+    }else{
+       fmt.Println("\nFolder Created\n"); 
     }
 
-   fmt.Println("\nFolder Created\n");
+   
 }
 
 func FileCreator(){
@@ -56,10 +60,28 @@ func FileCreator(){
 
   if fileWriter != nil {
 	log.Fatal(fileWriter);
-  }
+  }else{
     fmt.Println("\nFile Created\n")
 }
+    }
+
+type DeleteConfig struct {
+    fileName   string
+}
+func DeleteFile(){
+var del DeleteConfig
 
 
+fmt.Println("Escolha o nome do arquivo junto com a extensão\n exemplo: index.js");
+fmt.Scanln(&del.fileName)
+
+ ideleteFile := os.Remove(del.fileName)
+    if ideleteFile != nil {
+        log.Fatal(ideleteFile)
+    }else{
+    
+    fmt.Println("Foi Deletado o",del.fileName)
+        }
+}
 
 
